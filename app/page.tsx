9859a25eda7eda809739156ddef1fa526e73d74c
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2024-04-17 21:08:39 +0800

    edited background

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="md:w-9/12 mx-auto">
+    <div className="md:w-9/12 mx-auto text-gray-400 bg-zinc-900">
        <div className="my-8 sm:text-sm">
         <h1 className="flex flex-row min-h-1 justify-center items-center font-bold text-lg">Pua Xian Hao</h1>
         <p className=" text-sm flex flex-row min-h-1 justify-center items-center md:text-lg">S-18-18, Residensi HighPark Suites, 47301 Petaling Jaya</p>
