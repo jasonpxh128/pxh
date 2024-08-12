a56ada2a5a600f23a36ab1859ed5e47d8e8ddc39
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2024-08-12 16:49:03 +0800

    added new linkedin Article

--- app/page.tsx
+++ app/page.tsx
@@ -168,6 +168,49 @@ export default function Home() {
                 </div>
 
               </div>
+              
+              <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border border-muted p-3">
+                <div className="flex flex-col space-y-1.5">
+                  <div className="space-y-1">
+                    <h3 className="font-semibold tracking-tight text-base">
+                      <a href="https://www.linkedin.com/pulse/how-run-llama3-models-llamacpp-intel-gpus-sycl-oneapi-jason-pua-5g6vc/?trackingId=PgxN1cPCfuUKfgPvkIzveQ%3D%3D" target="blank" className="inline-flex items-center gap-1 hover:underline">Llama3 on Llama.cpp <span className="size-1 rounded-full bg-green-500"></span></a>
+                      
+                    </h3>
+                    <div className="hidden font-mono text-xs underline print:visible">
+                      https://www.linkedin.com/pulse/llama3-whisper-v3-long-text-summarization-intel-xeon-cpu-jason-pua-v66de/?trackingId=R2Nc1F5URmu41my4vImzOA%3D%3D
+                    </div>
+                    <p className="text-muted-foreground font-mono text-xs"> Llama 3.1 & Whisper v3 long text summarization on Intel Xeon CPU</p>
+                  </div>
+
+                </div>
+
+                <div className="text-pretty font-mono text-sm text-muted-foreground mt-auto flex">
+                  <div className="mt-2 flex flex-wrap gap-1">
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      Intel Demo
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      C++
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      Llama.cpp
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      OpenAI
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      CPU
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      OpenVINO
+                    </div>
+                    <div className="inline-flex items-center rounded-md border font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-white/80 text-black hover:bg-white/60 px-1 py-0 text-[15px]">
+                      HuggingFace
+                    </div>
+                  </div>
+                </div>
+
+              </div>
 
               <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border border-muted p-3">
                 <div className="flex flex-col space-y-1.5">
